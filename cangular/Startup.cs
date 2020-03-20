b0d4fdd2b0b0754bc23e94e5090ca712c26f7cbe
Sergey Lvov <sergey.lvov70@gmail.com>
2020-03-20 10:00:16 +0300

    init wiht ng serve in cmd

---
+++ cangular/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace cangular
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+
+			services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+			/*
+			services.AddHttpsRedirection(options =>
+			{
+				options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+				options.HttpsPort = 44381;
+			});
+			*/
+			/*
+			services.AddNodeServices(options =>
+			{
+				options.ProjectPath = "ClientApp";
+			});
+			*/
+
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "client/dist";
+			});
+
+			services.AddNodeServices();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHsts();
+
+			app.UseStaticFiles();
+			app.UseSpaStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action=Index}/{id?}");
+			});
+
+			app.UseSpa(spa =>
+			{
+				spa.Options.SourcePath = "client";
+				if (env.IsDevelopment())
+				{
+					//					spa.UseAngularCliServer("start");
+					spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+				}
+
+			});
+		}
+
+	}
+}
+			/*
+						app.Run(async (context) =>
+						{
+							await context.Response.WriteAsync("Hello World!");
+						});
+			*/
+		
+	
+
